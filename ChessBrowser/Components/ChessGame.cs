5c30fcf6f0cf4aa26398bcb3ced54718222863cc
Max Sone <pyaesone.max@gmail.com>
2025-02-26 20:10:19 -0700

    Implemented PGN FIle Parser

---
+++ ChessBrowser/Components/ChessGame.cs
@@ -0,0 +1,44 @@
+﻿namespace ChessBrowser.Components
+{
+    public class ChessGame
+    {
+        public readonly string eventName;
+        public readonly string site;
+        public readonly string roundNumber;
+        public readonly string whitePlayer;
+        public readonly string blackPlayer;
+        public readonly int whiteElo;
+        public readonly int blackElo;
+        public readonly char result;
+        public readonly DateTime eventDateTime;
+        public readonly string moves;
+            
+        public ChessGame(string eventName, string site, string roundNumber, string whitePlayer, string blackPlayer, int whiteElo, int blackElo, char result, DateTime eventDateTime, string moves)
+        {
+            this.eventName = eventName;
+            this.site = site;
+            this.roundNumber = roundNumber;
+            this.whitePlayer = whitePlayer;
+            this.blackPlayer = blackPlayer;
+            this.whiteElo = whiteElo;
+            this.blackElo = blackElo;
+            this.result = result;
+            this.eventDateTime = eventDateTime;
+            this.moves = moves;
+        }
+
+        public void printGame()
+        {
+            Console.WriteLine("Event Name: " + eventName);
+            Console.WriteLine("Site: " + site);
+            Console.WriteLine("Round: " + roundNumber);
+            Console.WriteLine("White: " + whitePlayer);
+            Console.WriteLine("Black: " + blackPlayer);
+            Console.WriteLine("White Elo: " + whiteElo);
+            Console.WriteLine("Black Elo: " + blackElo);
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("Date Time: " + eventDateTime);
+            Console.WriteLine("Moves: " + moves);
+        }
+    }
+}
---
+++ ChessBrowser/Components/PGNReader.cs
@@ -0,0 +1,114 @@
+﻿namespace ChessBrowser.Components
+{
+    public static class PGNReader
+    {
+        public static List<ChessGame> Read(string[] chessText)
+        {
+            List<ChessGame> chessGames = new List<ChessGame>();
+
+            int blankLineCounter = 0;
+
+            string eventName = "";
+            string site = "";
+            string round = "";
+            string whitePlayer = "";
+            string blackPlayer = "";
+            int whiteElo = 0;
+            int blackElo = 0;
+            char result = ' ';
+            DateTime eventDateTime = new DateTime();
+            string moves = "";
+
+
+            for (int i = 0; i < chessText.Length; i++)
+            {
+                if (chessText[i].StartsWith("[Event ")) {
+                    int strLen = chessText[i].Length;
+                    eventName = chessText[i].Substring(8, chessText[i].Length - 10);
+                } 
+                else if (chessText[i].StartsWith("[Site "))
+                {
+                    site = chessText[i].Substring(7, chessText[i].Length - 9);
+                }
+                else if (chessText[i].StartsWith("[Round "))
+                {
+                    round = chessText[i].Substring(8, chessText[i].Length - 10);
+                }
+                else if (chessText[i].StartsWith("[White "))
+                {
+                    whitePlayer = chessText[i].Substring(8, chessText[i].Length - 10);
+                }
+                else if (chessText[i].StartsWith("[Black "))
+                {
+                    blackPlayer = chessText[i].Substring(8, chessText[i].Length - 10);
+                }
+                else if (chessText[i].StartsWith("[Result "))
+                {
+                    string tempResult = chessText[i].Substring(9, chessText[i].Length - 11);
+
+                    if (tempResult == "1-0")
+                    {
+                        result = 'W';
+                    } else if (tempResult == "0-1")
+                    {
+                        result = 'B';
+                    } else
+                    {
+                        result = 'D';
+                    }
+                }
+                else if (chessText[i].StartsWith("[WhiteElo "))
+                {
+                    whiteElo = Int32.Parse(chessText[i].Substring(11, chessText[i].Length - 13));
+                }
+                else if (chessText[i].StartsWith("[BlackElo "))
+                {
+                    blackElo = Int32.Parse(chessText[i].Substring(11, chessText[i].Length - 13));
+                }
+                else if (chessText[i].StartsWith("[EventDate "))
+                {
+                    if (chessText[i].Contains('?'))
+                    {
+                        eventDateTime = new DateTime();
+                    } 
+                    else
+                    {
+                        eventDateTime = DateTime.Parse(chessText[i].Substring(12, chessText[i].Length - 14));
+                    }
+                }
+                else if (chessText[i] == "")
+                {
+                    blankLineCounter++;
+                    if (blankLineCounter % 2 == 0) {
+                        ChessGame chessGame = new ChessGame(
+                            eventName,
+                            site,
+                            round,
+                            whitePlayer,
+                            blackPlayer,
+                            whiteElo,
+                            blackElo,
+                            result,
+                            eventDateTime,
+                            moves
+                        );
+                        blankLineCounter = 0;
+                        chessGames.Add(chessGame);
+                        moves = "";
+                    }
+
+                }
+                else if (chessText[i].StartsWith("[ECO "))
+                {
+                    continue;
+                }
+                else
+                {
+                    moves += chessText[i];
+                }
+            }
+
+            return chessGames;
+        }
+    }
+}
